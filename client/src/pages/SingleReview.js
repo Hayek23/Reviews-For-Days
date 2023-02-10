facb3a9ac22431597291d1aba5b3776e85e3fbff
jwm <jwm3926@gmail.com>
2023-02-09 19:41:53 -0500

    more refators

--- client/src/pages/SingleReview.js
+++ client/src/pages/SingleReview.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+import ReviewList from '../components/ReviewList';
+import ReviewForm from '../components/ReviewForm';
 
 import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
 
-const SingleThought = () => {
+const SingleReview = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { thoughtId } = useParams();
 
@@ -46,13 +46,13 @@ const SingleThought = () => {
       </div>
 
       <div className="my-5">
-        <CommentList comments={thought.comments} />
+        <ReviewList comments={thought.comments} />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
+        <ReviewForm thoughtId={thought._id} />
       </div>
     </div>
   );
 };
 
-export default SingleThought;
+export default SingleReview;
