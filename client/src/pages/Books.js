75baa4add9c9c90b89eac7adf57a0fc7f5dc4955
Terry Boufis <terryboufis@gmail.com>
2023-02-13 19:56:47 -0600

    added to template

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -3,53 +3,116 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 
 import { ADD_BOOK_REVIEW } from '../../utils/mutations';
-import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+import { QUERY_BOOKS, QUERY_ME } from '../../utils/queries';
 
 import Auth from '../../utils/auth';
 
-
-function Books() {
-    const [errorMessage, setErrorMessage] = useState('');
-    const [author, setAuthor] = useState('');
-    const [title, setTitle] = useState('');
-
-    const handleInputChange = (event) => {
-        const { target } = event;
-        const inputType = target.name;
-        const inputValue = target.value;
     
-        if (event.target.value === '') {
-          setErrorMessage("You need to fill out all fields")
-        } 
+    const BookReview = () => {
+      const [bookReviewText, setBookReviewText] = useState('');
     
-        if (inputType === 'email') {
-          setAuthor(inputValue);
-        } else if (inputType === 'name') {
-          setTitle(inputValue);
+      const [characterCount, setCharacterCount] = useState(0);
+    
+      const [addBookReview, { error }] = useMutation(ADD_BOOK_REVIEW, {
+        update(cache, { data: { addBookReview } }) {
+          try {
+            const { bookReview } = cache.readQuery({ query: QUERY_BOOKS });
+    
+            cache.writeQuery({
+              query: QUERY_BOOKS,
+              data: { thoughts: [addBookReview, ...bookReview] },
+            });
+          } catch (e) {
+            console.error(e);
+          }
+    
+          // update me object's cache
+          const { me } = cache.readQuery({ query: QUERY_ME });
+          cache.writeQuery({
+            query: QUERY_ME,
+            data: { me: { ...me, bookReview: [...me.bookReview, addBookReview] } },
+          });
+        },
+      });
+    
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const { data } = await addBookReview({
+            variables: {
+              title,
+              genre,
+              readTime,
+              bookReviewText,
+              reviewAuthor: Auth.getProfile().data.username,
+            },
+          });
+    
+          setBookReviewText('');
+        } catch (err) {
+          console.error(err);
         }
-      }
-
-
-      
+      };
+    
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        if (name === 'bookReviewText' && value.length <= 280) {
+          setBookReviewText(value);
+          setCharacterCount(value.length);
+        }
+      };
+    
       return (
-        <div id="contact">
-          <form>
-            <input defaultValue={title} name = "Tile" placeholder="Title" onBlur={handleInputChange} type="email"/>
-            <input defaultValue={author} name = "Author" placeholder="Author" onBlur={handleInputChange} type="text"/>
-            <textarea placeholder="Your Review"></textarea>
-            <button onClick={handleFormSubmit}>Submit</button>
-          </form>
-        
-          {errorMessage && (
-              <div>
-                <p className="error-text">{errorMessage}</p>
-              </div>
-            )}
+        <div>
+          <h3>What are your thoughts on this book?</h3>
+    
+          {Auth.loggedIn() ? (
+            <>
+              <p
+                className={`m-0 ${
+                  characterCount === 280 || error ? 'text-danger' : ''
+                }`}
+              >
+                Character Count: {characterCount}/280
+              </p>
+              <form
+                className="flex-row justify-center justify-space-between-md align-center"
+                onSubmit={handleFormSubmit}
+              >
+                <div className="col-12 col-lg-9">
+                  <textarea
+                    name="thoughtText"
+                    placeholder="Here's a new review..."
+                    value={bookReviewText}
+                    className="form-input w-100"
+                    style={{ lineHeight: '1.5', resize: 'vertical' }}
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+    
+                <div className="col-12 col-lg-3">
+                  <button className="btn btn-primary btn-block py-3" type="submit">
+                    Add review
+                  </button>
+                </div>
+                {error && (
+                  <div className="col-12 my-3 bg-danger text-white p-3">
+                    {error.message}
+                  </div>
+                )}
+              </form>
+            </>
+          ) : (
+            <p>
+              Please login to share your review.{' '}
+              <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+            </p>
+          )}
         </div>
       );
-    }
-    
-    
+    };
     
     
     export default Books;
