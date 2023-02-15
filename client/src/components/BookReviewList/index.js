d0a2b71f9dafa201e9ef068de7bdae3a4c34135e
jwm <jwm3926@gmail.com>
2023-02-14 20:19:18 -0500

    commit

--- client/src/components/BookReviewList/index.js
+++ client/src/components/BookReviewList/index.js
@@ -7,9 +7,9 @@ const BookReviewList = ({
   showTitle = true,
   showUsername = true,
 }) => {
-//   if (!bookReviews.length) {
-//     return <h3>There are no book reviews! Be the first!</h3>;
-//   }
+  if (!bookReviews?.length) {
+    return <h3>There are no book reviews! Be the first!</h3>;
+  }
 
   return (
     <div>
